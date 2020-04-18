8ff7432cdae1ab1e3100bee28d4ea29aa73c62c2
Gabriele <gabbopeace@gmail.com>
2020-04-18 16:23:37 +0200

    Bug

--- src/gui/AvatarList.js
+++ src/gui/AvatarList.js
@@ -35,82 +35,82 @@ export default function AvatarList(onClickHandle) {
       <Grid container spacing={3}>
         <Grid item xs={6}  sm={4}>
           <Paper className={classes.paper}>
-             <Avatar onClick={onClickHandle(0)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-0.png'} />
+             <Avatar onClick={() => onClickHandle(0)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-0.png'} />
           </Paper>
         </Grid>
         <Grid item xs={6} sm={4}>
           <Paper className={classes.paper}>
-             <Avatar onClick={onClickHandle(1)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-1.png'} />
+             <Avatar onClick={() => onClickHandle(1)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-1.png'} />
           </Paper>
         </Grid>
         <Grid item xs={6} sm={4}>
           <Paper className={classes.paper}>
-             <Avatar onClick={onClickHandle(2)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-2.png'} />
+             <Avatar onClick={() => onClickHandle(2)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-2.png'} />
           </Paper>
         </Grid>
         <Grid item xs={6} sm={4}>
           <Paper className={classes.paper}>
-             <Avatar onClick={onClickHandle(3)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-3.png'} />
+             <Avatar onClick={() => onClickHandle(3)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-3.png'} />
           </Paper>
         </Grid>
         <Grid item xs={6} sm={4}>
           <Paper className={classes.paper}>
-             <Avatar onClick={onClickHandle(4)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-4.png'} />
+             <Avatar onClick={() => onClickHandle(4)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-4.png'} />
           </Paper>
         </Grid>
         <Grid item xs={6} sm={4}>
           <Paper className={classes.paper}>
-             <Avatar onClick={onClickHandle(5)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-5.png'} />
+             <Avatar onClick={() => onClickHandle(5)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-5.png'} />
           </Paper>
         </Grid>
         <Grid item xs={6} sm={4}>
           <Paper className={classes.paper}>
-             <Avatar onClick={onClickHandle(6)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-6.png'} />
+             <Avatar onClick={() => onClickHandle(6)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-6.png'} />
           </Paper>
         </Grid>
         <Grid item xs={6} sm={4}>
           <Paper className={classes.paper}>
-             <Avatar onClick={onClickHandle(7)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-7.png'} />
+             <Avatar onClick={() => onClickHandle(7)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-7.png'} />
           </Paper>
         </Grid>
         <Grid item xs={6} sm={4}>
           <Paper className={classes.paper}>
-             <Avatar onClick={onClickHandle(8)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-8.png'} />
+             <Avatar onClick={() => onClickHandle(8)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-8.png'} />
           </Paper>
         </Grid>
         <Grid item xs={6} sm={4}>
           <Paper className={classes.paper}>
-             <Avatar onClick={onClickHandle(9)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-9.png'} />
+             <Avatar onClick={() => onClickHandle(9)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-9.png'} />
           </Paper>
         </Grid>
         <Grid item xs={6} sm={4}>
           <Paper className={classes.paper}>
-             <Avatar onClick={onClickHandle(10)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-10.png'} />
+             <Avatar onClick={() => onClickHandle(10)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-10.png'} />
           </Paper>
         </Grid>
         <Grid item xs={6} sm={4}>
           <Paper className={classes.paper}>
-             <Avatar onClick={onClickHandle(11)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-11.png'} />
+             <Avatar onClick={() => onClickHandle(11)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-11.png'} />
           </Paper>
         </Grid>
         <Grid item xs={6} sm={4}>
           <Paper className={classes.paper}>
-             <Avatar onClick={onClickHandle(12)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-12.png'} />
+             <Avatar onClick={() => onClickHandle(12)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-12.png'} />
           </Paper>
         </Grid>
         <Grid item xs={6} sm={4}>
           <Paper className={classes.paper}>
-             <Avatar onClick={onClickHandle(13)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-13.png'} />
+             <Avatar onClick={() => onClickHandle(13)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-13.png'} />
           </Paper>
         </Grid>
         <Grid item xs={6} sm={4}>
           <Paper className={classes.paper}>
-             <Avatar onClick={onClickHandle(14)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-14.png'} />
+             <Avatar onClick={() => onClickHandle(14)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-14.png'} />
           </Paper>
         </Grid>
         <Grid item xs={6} sm={4}>
           <Paper className={classes.paper}>
-             <Avatar onClick={onClickHandle(15)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-15.png'} />
+             <Avatar onClick={() => onClickHandle(15)} className={classes.avatar} alt="Natacha" src={process.env.PUBLIC_URL + '/avatars/png/avatar-15.png'} />
           </Paper>
         </Grid>
       </Grid>
